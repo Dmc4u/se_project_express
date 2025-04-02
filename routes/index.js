4e19b7f390b3046ebbc5c52b453aa6cd458a6e5a
Dmc4u <dmsesbiz2005@gmail.com>
2025-04-02 18:06:06 +0300

    Final Commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
 
 // Handle non-existent routes
 router.use((req, res) => {
-  res.status(404).send({ message: "Requested resource not found" });
+  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
