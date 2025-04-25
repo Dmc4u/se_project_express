4bf979ad3e93e6973e6686c491fc52fb0f7d61fe
Dmc4u <dmsesbiz2005@gmail.com>
2025-04-25 19:51:23 +0300

    Update

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { getCurrentUser, updateUser } = require("../controllers/users");
-const { validateUserBody } = require("../middlewares/validation");
+const { validateUserBody, validateId } = require("../middlewares/validation");
 
 const router = express.Router();
 
-router.get("/me", getCurrentUser); //ask question about validateId that is not used in this route
+router.get("/me", validateId, getCurrentUser); //ask question about validateId that is not used in this route
 // router.get("/me", validateId, getCurrentUser); //ask question about validateId that is not used in this route
 router.patch("/me", validateUserBody, updateUser);
 
